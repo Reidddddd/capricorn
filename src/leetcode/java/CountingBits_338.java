05c82b9d224ddfe012adb8e86f8f18742a9a4a85
Reid <reidchan@qq.com>
2016-06-27 12:02:28 +0800

    leetcode java 338

---
+++ src/leetcode/java/CountingBits_338.java
@@ -0,0 +1,15 @@
+package leetcode.java;
+
+public class CountingBits_338 {
+  // Take number X for example, 10011001. 
+  // Divide it in 2 parts:
+  // <1>.the last digit (1 or 0, which is (i&1), == (i%2) )
+  // <2>.the other digits(the number of 1 is f[i >> 1], == f[i/2])
+  public int[] countBits(int num) {
+    int[] bits = new int [num + 1];
+    for (int i = 1; i <= num; i++) {
+      bits[i] = bits[i >> 1] + i & 1;
+    }
+    return bits;
+  }
+}

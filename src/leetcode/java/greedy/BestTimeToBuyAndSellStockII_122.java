f840c5f0771446912953001cec6ae9cc8abed238
Reid <reidddchan@outlook.com>
2016-08-10 15:33:01 +0800

    greedy 122 and 376

---
+++ src/leetcode/java/greedy/BestTimeToBuyAndSellStockII_122.java
@@ -0,0 +1,11 @@
+package leetcode.java.greedy;
+
+public class BestTimeToBuyAndSellStockII_122 {
+  public int maxProfit(int[] prices) {
+    int maxProfit = 0;
+    for (int i = 1; i < prices.length; i++) {
+      if (prices[i] - prices[i - 1] >= 0) maxProfit += prices[i] - prices[i - 1];
+    }
+    return maxProfit;
+  }
+}

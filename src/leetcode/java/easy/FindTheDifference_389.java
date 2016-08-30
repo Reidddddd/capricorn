702da851243001e73c6ea23ae53b185ba6cc9649
Reid <reidddchan@outlook.com>
2016-08-30 22:49:17 +0800

    LEETCODE-00389 Easy: Find The Difference

---
+++ src/leetcode/java/easy/FindTheDifference_389.java
@@ -0,0 +1,17 @@
+package leetcode.java.easy;
+
+public class FindTheDifference_389 {
+  public char findTheDifference(String s, String t) {
+    int[] ch = new int[256];
+    for (char c : s.toCharArray()) ch[c] += 1;
+    char car = '\u0000';
+    for (char c : t.toCharArray()) {
+      ch[c]--;
+      if (ch[c] < 0) {
+        car = c;
+        break;
+      }
+    }
+    return car;
+  }
+}

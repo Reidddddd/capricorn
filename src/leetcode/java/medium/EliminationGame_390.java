8088a9ce8c10f58ae28082648c704aab57a88264
Reid <reidddchan@outlook.com>
2016-08-31 13:50:57 +0800

    LEETCODE-00390 Medium: Elimination Game

---
+++ src/leetcode/java/medium/EliminationGame_390.java
@@ -0,0 +1,23 @@
+package leetcode.java.medium;
+
+public class EliminationGame_390 {
+  public int lastRemaining(int n) {
+    boolean left = true;
+    int step = 1, head = 1;
+    int remaining = n;
+    while (remaining > 1) {
+      if (left || remaining % 2 == 1) {
+        head = head + step;
+      }
+      left = !left;
+      remaining /= 2;
+      step *= 2;
+    }
+    return 0;
+  }
+
+  public static void main(String[] args) {
+    EliminationGame_390 eg = new EliminationGame_390();
+    System.out.println(eg.lastRemaining(18));
+  }
+}

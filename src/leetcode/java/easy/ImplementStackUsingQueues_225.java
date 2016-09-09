1e61c017518f3826000c6d0dfdd319d5482f870a
Reid <reidddchan@outlook.com>
2016-09-09 21:14:59 +0800

    LEETCODE-00225 Easy: Implement Stack Using Queues

---
+++ src/leetcode/java/easy/ImplementStackUsingQueues_225.java
@@ -0,0 +1,27 @@
+package leetcode.java.easy;
+
+import java.util.LinkedList;
+
+public class ImplementStackUsingQueues_225 {
+  LinkedList<Integer> stack = new LinkedList<>();
+
+  // Push element x onto stack.
+  public void push(int x) {
+    stack.addLast(x);
+  }
+
+  // Removes the element on top of the stack.
+  public void pop() {
+    stack.removeLast();
+  }
+
+  // Get the top element.
+  public int top() {
+    return stack.getLast();
+  }
+
+  // Return whether the stack is empty.
+  public boolean empty() {
+    return stack.isEmpty();
+  }
+}

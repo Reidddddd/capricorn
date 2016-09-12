60f5399f5481c7be1f841efb746896a0845961c2
Reid <reidddchan@outlook.com>
2016-09-12 23:24:21 +0800

    LEETCODE-00116 Medium: Populating Next Right Pointers in Each Node

---
+++ src/leetcode/java/medium/PopulatingNextRightPointersInEachNode_116.java
@@ -0,0 +1,24 @@
+package leetcode.java.medium;
+
+public class PopulatingNextRightPointersInEachNode_116 {
+  public void connect(TreeLinkNode root) {
+    if (root == null) return;
+    root.next = null;
+    nodeConnect(root.left, root.right);
+  }
+
+  public static void nodeConnect(TreeLinkNode left, TreeLinkNode right) {
+    if (left == null) return;
+    left.next = right;
+    right.next = null;
+    nodeConnect(left.left, left.right);
+    nodeConnect(left.right, right.left);
+    nodeConnect(right.left, right.right);
+  }
+
+  class TreeLinkNode {
+    int val;
+    TreeLinkNode left, right, next;
+    TreeLinkNode(int x) { val = x; }
+  }
+}

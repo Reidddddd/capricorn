c13e06e9fcde1e73ccdc385f78a725422f38400a
Reid <reidddchan@outlook.com>
2016-10-31 20:50:10 +0800

    LEETCODE-00343 Medium: Integer Break

---
+++ src/leetcode/java/medium/IntegerBreak_343.java
@@ -0,0 +1,17 @@
+package leetcode.java.medium;
+
+public class IntegerBreak_343 {
+  public int integerBreak(int n) {
+    int[] dp = new int[n + 1];
+    dp[1] = 1;
+    int breakPoint;
+    for (int i = 2; i <= n; i++) {
+      breakPoint = i / 2;
+      dp[i] =
+          Math.max(
+            Math.max(dp[breakPoint], breakPoint) * Math.max(dp[i - breakPoint], i - breakPoint),
+            Math.max(dp[breakPoint - 1], breakPoint - 1) * Math.max(dp[i - breakPoint + 1], i - breakPoint + 1));
+    }
+    return dp[n];
+  }
+}

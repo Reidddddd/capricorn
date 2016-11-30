7c46e186b9e7d35498b26ccb1cae509995c0ac5d
Reid <reidddchan@outlook.com>
2016-12-01 00:18:22 +0800

    LEETCODE-00102 Easy: Binary Tree Level Order Traversal

---
+++ src/leetcode/java/easy/BinaryTreeLevelOrderTraversal_102.java
@@ -0,0 +1,33 @@
+package leetcode.java.easy;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class BinaryTreeLevelOrderTraversal_102 {
+  public List<List<Integer>> levelOrder(TreeNode root) {
+    List<List<Integer>> result = new LinkedList<>();
+    if (root == null) return result;
+    LinkedList<TreeNode> queue = new LinkedList<>();
+    queue.addLast(root);
+    int size = 0;
+    TreeNode node = null;
+    while ((size = queue.size()) != 0) {
+      LinkedList<Integer> level = new LinkedList<>();
+      for (int i = 0; i < size; i++) {
+        node = queue.removeFirst();
+        level.add(node.val);
+        if (node.left != null) queue.addLast(node.left);
+        if (node.right != null) queue.addLast(node.right);
+      }
+      ((LinkedList) result).addLast(level);
+    }
+    return result;
+  }
+}
+
+class TreeNode {
+  int val;
+  TreeNode left;
+  TreeNode right;
+  TreeNode(int x) { val = x; }
+}

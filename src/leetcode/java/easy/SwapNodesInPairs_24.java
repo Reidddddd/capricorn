88559ac90321ce9217bd1f7910107a8d0e517811
Reid <reidddchan@outlook.com>
2016-12-03 00:54:40 +0800

    LEETCODE-00024 Easy: Swap Nodes in Pairs

---
+++ src/leetcode/java/easy/SwapNodesInPairs_24.java
@@ -0,0 +1,19 @@
+package leetcode.java.easy;
+
+public class SwapNodesInPairs_24 {
+  public ListNode swapPairs(ListNode head) {
+    if (head == null) return null;
+    if (head.next == null) return head;
+    head.next.next = swapPairs(head.next.next);
+    ListNode h = head.next;
+    head.next = h.next;
+    h.next = head;
+    return h;
+  }
+}
+
+class ListNode {
+  int val;
+  ListNode next;
+  ListNode(int x) { val = x; }
+}

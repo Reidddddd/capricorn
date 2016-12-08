4355fc57d07cc619a1d52b153c9a98753eec62e8
Reid <reidddchan@outlook.com>
2016-12-09 00:03:10 +0800

    LEETCODE-00066 Easy: Plus One

---
+++ src/leetcode/python/easy/PlusOne_66.py
@@ -0,0 +1,14 @@
+class PlusOne_66(object):
+    def plusOne(self, digits):
+        """
+        :type digits: List[int]
+        :rtype: List[int]
+        """
+        i = len(digits) - 1
+        while i >= 0:
+            if digits[i] < 9:
+                digits[i] += 1
+                return digits
+            digits[i] = 0
+            i -= 1
+        return [1] + digits

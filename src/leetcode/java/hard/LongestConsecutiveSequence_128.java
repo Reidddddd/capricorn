249d28d03f5c1c338b6dc81be483022b9eab6c14
Reid <reidddchan@outlook.com>
2016-12-14 23:20:03 +0800

    LEETCODE-00128 Hard: Longest Consecutive Sequence

---
+++ src/leetcode/java/hard/LongestConsecutiveSequence_128.java
@@ -0,0 +1,21 @@
+package leetcode.java.hard;
+
+import java.util.HashMap;
+
+public class LongestConsecutiveSequence_128 {
+  public int longestConsecutive(int[] nums) {
+    HashMap<Integer, Integer> map = new HashMap<>();
+    int res = 0;
+    for (int i : nums) {
+      if (!map.containsKey(i)) {
+        int left = map.containsKey(i - 1) ? map.get(i - 1) : 0;
+        int right = map.containsKey(i + 1) ? map.get(i + 1) : 0;
+        int sum = left + right + 1;
+        map.put(i, sum);
+        res = Math.max(res, sum);
+        map.put(i - left, sum);
+        map.put(i + right, sum);
+      }
+    }
+  }
+}

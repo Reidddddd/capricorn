fa4b252003c82a9e8ddeed69019d8ca960930a58
Reid <reidddchan@outlook.com>
2016-12-25 01:09:45 +0800

    LEETCODE-00057 Hard: Insert Interval

---
+++ src/leetcode/java/hard/InsertInterval_57.java
@@ -0,0 +1,45 @@
+package leetcode.java.hard;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class InsertInterval_57 {
+  public List<Interval> insert(List<Interval> intervals, Interval newInterval) {
+    List<Interval> left = new LinkedList<>();
+    List<Interval> right = new LinkedList<>();
+    if (intervals == null || intervals.isEmpty()) {
+      left.add(newInterval);
+      return left;
+    }
+    if (newInterval == null) return intervals;
+    int start = newInterval.start, end = newInterval.end;
+    for (Interval itv : intervals) {
+      if (itv.end < start) {
+        left.add(itv);
+      } else if (itv.start > end) {
+        right.add(itv);
+      } else {
+        start = Math.min(itv.start, start);
+        end = Math.max(itv.end, end);
+      }
+    }
+    left.add(new Interval(start, end));
+    left.addAll(right);
+    return left;
+  }
+
+  class Interval {
+    int start;
+    int end;
+
+    Interval() {
+      start = 0;
+      end = 0;
+    }
+
+    Interval(int s, int e) {
+      start = s;
+      end = e;
+    }
+  }
+}
---
+++ src/leetcode/python/hard/InsertInterval_57.py
@@ -0,0 +1,24 @@
+class Interval(object):
+    def __init__(self, s=0, e=0):
+        self.start = s
+        self.end = e
+
+
+class InsertInterval_57(object):
+    def insert(self, intervals, newInterval):
+        """
+        :type intervals: List[Interval]
+        :type newInterval: Interval
+        :rtype: List[Interval]
+        """
+        s, e = newInterval.start, newInterval.end
+        left, right = [], []
+        for i in intervals:
+            if i.end < s:
+                left += i,
+            elif i.start > e:
+                right += i,
+            else:
+                s = min(i.start, s)
+                e = max(i.end, e)
+        return left + [Interval(s, e)] + right

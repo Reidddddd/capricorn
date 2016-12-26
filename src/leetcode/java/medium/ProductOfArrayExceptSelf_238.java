f7e0ab5de1ec38ec3c28a6f61087fe1648f5e4c1
Reid <reidddchan@outlook.com>
2016-12-27 00:01:15 +0800

    LEETCODE-XXXXX Update: Product of Array Except Self

--- src/leetcode/java/medium/ProductOfArrayExceptSelf_238.java
+++ src/leetcode/java/medium/ProductOfArrayExceptSelf_238.java
@@ -7,7 +7,11 @@ public class ProductOfArrayExceptSelf_238 {
     for (int i = 1; i < nums.length; i++) {
       leftProduct[i] = leftProduct[i - 1] * nums[i - 1];
     }
-    
+    int rightProduct = 1;
+    for (int i = nums.length - 1; i >= 0; i--) {
+      leftProduct[i] *= rightProduct;
+      rightProduct *= nums[i];
+    }
     return leftProduct;
   }
 }

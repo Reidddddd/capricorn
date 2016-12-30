206dbce7c61a9c8e3c5253248e91bf09ae40aa8d
Reid <reidddchan@outlook.com>
2016-12-31 00:35:46 +0800

    LEETCODE-XXXXX Update: Update Largest Number Python version

--- src/leetcode/python/medium/LargestNumber_179.py
+++ src/leetcode/python/medium/LargestNumber_179.py
@@ -4,10 +4,3 @@ class LargestNumber_179(object):
     def largestNumber(self, nums):
         r = ''.join(sorted(map(str, nums), lambda a,b: [1, -1][a+b>b+a]))
         return r.lstrip('0') or '0'
-
-
-l = LargestNumber_179()
-a = [1, 2, 3, 4]
-print(l.largestNumber(a))
-print(str(a))
-print(''.join(str(i) for i in a))

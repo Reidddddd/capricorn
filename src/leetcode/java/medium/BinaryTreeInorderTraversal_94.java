7488d7dcfe04f07e39b18a61f5b388bc4a7e56ee
Reid <reidddchan@outlook.com>
2017-01-10 00:23:06 +0800

    LEETCODE-00094 Medium: Binary Tree Inorder Traversal

---
+++ src/leetcode/java/medium/BinaryTreeInorderTraversal_94.java
@@ -0,0 +1,26 @@
+package leetcode.java.medium;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BinaryTreeInorderTraversal_94 {
+  public List<Integer> inorderTraversal(TreeNode root) {
+    List<Integer> list = new ArrayList<Integer>();
+    addToList(root, list);
+    return list;
+  }
+
+  public static void addToList(TreeNode root, List<Integer> list) {
+    if (root == null) return;
+    addToList(root.left, list);
+    if (root.val != '#') list.add(root.val);
+    addToList(root.right, list);
+  }
+
+  public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode(int x) { val = x; }
+  }
+}

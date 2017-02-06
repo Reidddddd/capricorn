a90bbd64b9677584853314c49d5b0142b01cc4a6
Reid <reidddchan@outlook.com>
2017-02-06 22:39:05 +0800

    LEETCODE-00274 Medium: H Index

---
+++ src/leetcode/java/medium/HIndex_274.java
@@ -0,0 +1,19 @@
+package leetcode.java.medium;
+
+public class HIndex_274 {
+  public int hIndex(int[] citations) {
+    int length = citations.length;
+    if (length == 0) return 0;
+    int[] h = new int[length + 1];
+    for (int i = 0; i < length; i++) {
+      if (citations[i] > length) h[length]++;
+      else h[citations[i]]++;
+    }
+    int cnt = 0;
+    for (int i = length; i >= 0; i--) {
+      cnt += h[i];
+      if (cnt >= i) return i;
+    }
+    return 0;
+  }
+}

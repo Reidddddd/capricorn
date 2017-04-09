8f7a5a80b8864fc0aa31720327a44d00e3e3b276
Reid <reidddchan@outlook.com>
2017-04-09 23:39:16 +0800

    LEETCODE-XXXXX Update: Simplify Leetcode 557 in Java Version

--- src/leetcode/java/ReverseWordsInAStringIII_557.java
+++ src/leetcode/java/ReverseWordsInAStringIII_557.java
@@ -5,22 +5,10 @@ public class ReverseWordsInAStringIII_557 {
     String[] splits = s.split(" ");
     StringBuilder builder = new StringBuilder();
     for (int i = 0; i < splits.length; i++) {
-      builder.append(reverse(splits[i]));
+      builder.append(new StringBuilder(splits[i]).reverse().toString());
       if (i == splits.length - 1) break;
       builder.append(" ");
     }
     return builder.toString();
-  }
-
-  private String reverse(String word) {
-    int len = word.length();
-    char[] ch = word.toCharArray();
-    char tmp;
-    for (int i = 0; i < len / 2; i++) {
-      tmp = ch[i];
-      ch[i] = ch[len - i - 1];
-      ch[len - i - 1] = tmp;
-    }
-    return new String(ch);
   }
 }

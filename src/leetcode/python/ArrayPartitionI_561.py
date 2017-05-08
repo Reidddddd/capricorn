2daf97d1bd36c5c3119dd73f42fbd5725b64ec35
Reid <reidddchan@outlook.com>
2017-05-08 22:52:00 +0800

    LEETCODE-000561 Easy: Array Partition I

---
+++ src/leetcode/python/ArrayPartitionI_561.py
@@ -0,0 +1,7 @@
+class ArrayPartitionI_561(object):
+    def arrayPairSum(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        return sum(sorted(nums)[::2])

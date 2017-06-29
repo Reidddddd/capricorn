c8d9ea1c56a37d4be9fcf176c7e3476f8876aadc
Reid <reidddchan@outlook.com>
2017-06-29 21:47:48 +0800

    LEETCODE-00160 Easy: Intersection of Two LinkedLists

---
+++ src/leetcode/java/IntersectionOfTwoLinkedLists_160.java
@@ -0,0 +1,24 @@
+package leetcode.java;
+
+public class IntersectionOfTwoLinkedLists_160 {
+  public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+    if (headA == null || headB == null) return null;
+    ListNode a = headA;
+    ListNode b = headB;
+    while (a != b) {
+      a = a == null ? headB : a.next;
+      b = b == null ? headA : b.next;
+    }
+    return a;
+  }
+
+  class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int x) {
+      val = x;
+      next = null;
+    }
+  }
+}
